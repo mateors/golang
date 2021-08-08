5a1713b048ae7a8ed7db518a4f7f0ad9f2f94881
Mostain Billah <bill.rassel@gmail.com>
2021-08-08 10:06:50 +0600

    go embed with gin framework

--- main.go
+++ main.go
@@ -5,29 +5,51 @@ import (
 	_ "embed"
 	"fmt"
 	"net/http"
+	"path"
 
 	"github.com/gin-gonic/gin"
 )
 
 var router *gin.Engine
 
-//go:embed template
-var tem embed.FS
+//go:embed assets/*
+var staticFS embed.FS
 
 func init() {
 	router = gin.Default()
-	fmt.Println(">>", tem)
+
+	// de, err := tem.ReadDir("assets")
+	// fmt.Println(">>", err)
+	// for k, v := range de {
+
+	// 	fs, _ := v.Info()
+	// 	fmt.Println(k, v.Name(), fs.Name(), fs.Size())
+	// }
 	//gin.SetMode(gin.ReleaseMode) //
+
+	//fmt.Println(path.Join("/assets/", "/resources"))
+
 }
 
 func main() {
 
-	//var f embed.FS
-	//f.ReadDir()
-	//fmt.Println(f)
-
 	//gin.HandlerFunc
 	router.LoadHTMLGlob("template/*")
+
+	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(tem))))
+	//fs := http.FileServer(tem)
+	//router.StaticFS("/resources", http.FS(tem))
+
+	router.GET("/:foldername/*filepath", func(c *gin.Context) {
+
+		fmt.Println("folder:", c.Param("foldername"))
+
+		for key, val := range c.Params {
+			fmt.Println(key, "==", val)
+		}
+
+		c.FileFromFS(path.Join("/assets/", c.Request.URL.Path), http.FS(staticFS))
+	})
 
 	router.GET("/", func(c *gin.Context) {
 
